faebe846a46ee5691116281848e0c166ed9939b1
Tim Lebel <tim.lebel@gmail.com>
2017-08-10 01:21:08 -0700

    Started on prompt

---
+++ prompt.go
@@ -0,0 +1,103 @@
+package fansi
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+// https://github.com/mgutz/ansi/blob/master/ansi.go
+type PromptOptions struct {
+	in  io.Reader
+	out io.Writer
+}
+
+// Prompt encapsulates the functionality necessary to prompt
+// the user for various kinds of inputs in an interactive program
+type Prompt struct {
+	in  *bufio.Reader
+	out *bufio.Writer
+}
+
+// NewPrompt creates an instance of Prompt
+func NewPrompt(opts PromptOptions) *Prompt {
+	return &Prompt{
+		in:  bufio.NewReader(opts.in),
+		out: bufio.NewWriter(opts.out),
+	}
+}
+
+func NewStdPrompt() *Prompt {
+	return NewPrompt(PromptOptions{
+		in:  os.Stdin,
+		out: os.Stdout,
+	})
+}
+
+func NewTestPrompt(input string) *Prompt {
+	inbuf := bytes.NewBufferString(input)
+	return NewPrompt(PromptOptions{
+		in:  inbuf,
+		out: os.Stdout,
+	})
+}
+
+func (p *Prompt) input(s string) error {
+	n, err := p.in.Read([]byte(s))
+	if n != len(s) {
+		return fmt.Errorf("Only %d of %d bytes were read", n, len(s))
+	}
+	return err
+}
+
+// GetInput prints the message and prompts the user for string input.
+// It returns the input up until a newline, or an error if an interrupt
+// signal was sent.
+func (p *Prompt) GetInput(msg string) (string, error) {
+	fmt.Println(msg)
+	fmt.Print("-> ")
+	text, err := p.in.ReadString('\n')
+	return strings.TrimSpace(text), err
+}
+
+// GetSecretInput works like GetInput, but displays a configurable
+// character instead of the current buffer, in the style of a password
+// input.
+func (p *Prompt) GetSecretInput(msg string) (string, error) {
+	ok := false
+	bytePassword := []byte{}
+
+	for !ok {
+		fmt.Println(msg)
+		fmt.Print("-> ")
+		bytePassword, _ = terminal.ReadPassword(int(syscall.Stdin))
+		fmt.Println()
+
+		fmt.Println("Enter again to confirm")
+		fmt.Print("-> ")
+		bytePasswordConfirm, _ := terminal.ReadPassword(int(syscall.Stdin))
+		fmt.Println()
+
+		ok = bytes.Compare(bytePassword, bytePasswordConfirm) == 0
+		if !ok {
+			fmt.Println("Entries did not match!")
+		}
+	}
+
+	return strings.TrimSpace(string(bytePassword)), nil
+}
+
+func (p *Prompt) AskYesNo(msg string) bool {
+	fmt.Fprintf(p.out, "%s [Y/n] ", msg)
+	p.out.Flush()
+	text, _ := p.in.ReadString('\n')
+	// convert CRLF to LF
+	text = strings.Replace(text, "\n", "", -1)
+	return !strings.Contains(strings.ToLower(text), "n")
+}
---
+++ prompt_test.go
@@ -0,0 +1,29 @@
+package fansi
+
+import "testing"
+import "strings"
+
+func askYesNoTest(t *testing.T, input string, expect bool) {
+	p := NewTestPrompt(input)
+	ans := p.AskYesNo("")
+	if ans != expect {
+		t.Errorf("%s should have produced %v, but didn't",
+			strings.TrimSpace(input), expect)
+	}
+}
+
+func TestAskYesNo(t *testing.T) {
+	// Responds correctly to yes
+	askYesNoTest(t, "Y\n", true)
+	askYesNoTest(t, "y\n", true)
+	askYesNoTest(t, "yYy\n", true)
+	askYesNoTest(t, "yes\n", true)
+	askYesNoTest(t, "YES\n", true)
+	// Responds correctly to no
+	askYesNoTest(t, "N\n", false)
+	askYesNoTest(t, "n\n", false)
+	askYesNoTest(t, "nnn\n", false)
+	askYesNoTest(t, "no\n", false)
+	askYesNoTest(t, "NO\n", false)
+	askYesNoTest(t, "NONONONONONO\n", false)
+}
